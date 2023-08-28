a7219e0c34f4f4952d2799ea02dedd40c3938e11
ircamitoc <iraincamitoc@gmail.com>
2023-08-28 20:36:35 +0800

    Revised line

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Homepage.");
+});
+
 app.listen(PORT, () =>
   console.log(`Server Running on port: http://localhost:${PORT}`)
 );
