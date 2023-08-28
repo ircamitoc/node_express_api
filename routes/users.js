ba43cf3f7b88bbaee054ff0c89823dace806db65
ircamitoc <iraincamitoc@gmail.com>
2023-08-28 22:10:21 +0800

    Added line of code

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,7 @@ import express from "express";
 import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
-];
+const users = [];
 
 // Route to get user data
 router.get("/", (req, res) => {
@@ -26,6 +15,10 @@ router.post("/", (req, res) => {
   users.push({ ...user, id: uuidv4() });
 
   res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+router.get("/:id", (req, res) => {
+  res.send("THE GET ID ROUTE");
 });
 
 export default router;
