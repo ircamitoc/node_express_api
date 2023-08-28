bcfcba967256fa8b9616b1be30032185fbfd815e
ircamitoc <iraincamitoc@gmail.com>
2023-08-28 21:02:15 +0800

    Revised code

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,18 @@ const users = [
     lastName: "Doe",
     age: 25,
   },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24,
+  },
 ];
 
 // Route to get user data
 router.get("/", (req, res) => {
   res.json(users); // Return JSON data
 });
+
+router.post("/", (req, res) => {});
 
 export default router;
