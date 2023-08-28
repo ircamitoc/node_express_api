0bdef07e3e6647118f70a6fb30c40859872d30fa
ircamitoc <iraincamitoc@gmail.com>
2023-08-28 22:05:57 +0800

    Added uuid and lines of code

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
-// users.js
 import express from "express";
-
+import { v4 as uuidv4 } from "uuid";
 const router = express.Router();
 
 const users = [
@@ -22,9 +21,11 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log("POST ROUTE REACHED");
+  const user = req.body;
 
-  res.send("POST ROUTE REACHED");
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
 });
 
 export default router;
